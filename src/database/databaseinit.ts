e4a672cd4606ee730939ad8401315b0da16a8b1f
thiago cavalcante <thiagocavalcante372@gmail.com>
2024-04-29 13:29:32 -0300

    first commit

---
+++ src/database/databaseinit.ts
@@ -0,0 +1,22 @@
+import { SQLiteDatabase } from "expo-sqlite/next"
+
+export async function databaseInit(database:SQLiteDatabase) {
+    await database.execAsync(`
+    
+        PRAGMA journal_mode = 'wal';
+
+        CREATE TABLE IF NOT EXISTS goals (
+            id INTEGER PRIMARY KEY NOT NULL,
+            name  TEXT NOT NULL,
+            total REAL NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS transactions (
+            id  INTEGER PRIMARY KEY NOT NULL,
+            goal_id INTEGER,
+            amount REAL NOT NULL,
+            create_at DEFAULT CURRENT_TIMESTAMP
+        );
+    
+    `)
+}
